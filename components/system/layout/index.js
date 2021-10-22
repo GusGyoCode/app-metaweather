ea3be0c5ddf5b5ef8f31a5481a4895b4f0781e3d
Gustavo Lucena <guspibe15@gmail.com>
2021-10-22 08:56:25 -0400

    adding new styles for layout

--- components/system/layout/index.js
+++ components/system/layout/index.js
@@ -10,7 +10,7 @@ export default function Layout({titlePage, children}) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <div className="w-full">
+      <div className="w-full flex flex-wrap">
         {children}
       </div>
     </>
---
+++ components/system/main/index.js
@@ -0,0 +1,7 @@
+export default function Main () {
+  return (
+    <main className="w-full lg:w-3/4 h-screen bg-morado-oscuro overflow-hidden overflow-y-auto">
+
+    </main>
+  )
+}
