f8b0a51aa2cd8d970200e70c3aeedcc7d09bfb89
Gustavo Lucena <guspibe15@gmail.com>
2021-10-26 15:09:55 -0400

    adding new file notification

---
+++ components/system/notification/index.js
@@ -0,0 +1,9 @@
+export default function Notification () {
+  return (
+    <div className="absolute top-0 left-0 w-full h-screen bg-black z-50 bg-opacity-50 flex justify-center items-center">
+      <div className="w-96 p-4 rounded-xl bg-white">
+
+      </div>
+    </div>
+  )
+}
